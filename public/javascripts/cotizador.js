0b39ef1a00918d06c291d56ec5e6c7f9d0ae0c95
maf248 <maf_248@hotmail.com>
2021-04-21 12:11:52 -0300

    Completé la logica basica por JS front para que muestre el requisito de cantidad de mails al tildar "Email marketing" y ocultarlo caso contrario. Además valida que se seleccionen todas las opciones requeridas para mostrar el contenedor con los resultados, caso contrario oculta los resultados si faltase seleccionar algo

--- public/javascripts/cotizador.js
+++ public/javascripts/cotizador.js
@@ -1,7 +1,35 @@
 const qs = (text) => document.querySelector(text);
 const qsa = (text) => document.querySelectorAll(text);
 
-/*---Se capturan los checkboxes de los servicios requeridos---*/
+var completeForm = [false, false, false, false];
+
+/*---Se captura el selector de país de los proveedores--*/
+var countrySupplier = qs('#country-supplier');
+    if (countrySupplier.value != '') {
+        completeForm[0] = true;
+    }
+
+countrySupplier.addEventListener('change', function() {
+    console.log(this.value)
+    if (countrySupplier.value != '') {
+        completeForm[0] = true;
+    }
+});
+/*---Se capturan los checkboxes en conjunto, de los servicios requeridos---*/
+var checkboxesServices = qsa('input[type=checkbox]');
+var checkboxesValidate = [false, false, false, false, false, false, false, false, false];
+
+checkboxesServices.forEach((checkboxService, i) => {
+    checkboxService.addEventListener('change', function() {
+        if (this.checked) {
+            checkboxesValidate[i] = true;
+        } else {
+            checkboxesValidate[i] = false;
+        }
+    });
+})
+
+/*---Se capturan los checkboxes individualmente, de los servicios requeridos---*/
 var googleFacebookAds = qs('#google-facebook-ads');
 var seo = qs('#seo');
 var wordpress = qs('#wordpress');
@@ -23,6 +51,46 @@ email.addEventListener('change', function() {
     }
   });
 
+/*---Se captura el selector de país de los anuncios--*/
+var countryAnnounce = qs('#country-announce');
+    if (countryAnnounce.value != '') {
+        completeForm[2] = true;
+    }
+
+countryAnnounce.addEventListener('change', function() {
+    console.log(this.value)
+    if (countryAnnounce.value != '') {
+        completeForm[2] = true;
+    }
+});
+
+/*---Se capturan los inputs radio que determinan el tipo de objetivo en la campaña de marketing---*/
+var objectives = qsa('input[name="objetivo"]');
+/*-Se recorren para detectar cuando alguno de los 3 es tildado-*/
+objectives.forEach(objective => {
+    objective.addEventListener('change', function() {
+        if (this.checked) {
+            completeForm[3] = true;
+        }
+    });
+})
+
+/*---Se captura el bloque que muestra los resultados, y se muestra solamente al seleccionar todas las opciones requeridas---*/
 var resultsContainer = qs('#results-container');
-resultsContainer.style.display = 'none';
+/*--Evento general para validar si mostrar resultados o no, en base a lo seleccionado---*/
+window.addEventListener('change', function() {
+    if (checkboxesValidate.includes(true)) {
+        completeForm[1] = true;
+    } else {
+        completeForm[1] = false;
+    }
+    if(completeForm.includes(false)) {
+        resultsContainer.style.display = 'none';
+    } else {
+        resultsContainer.style.display = 'block';
+    }
+    console.log(completeForm)
+});
+
+
 
