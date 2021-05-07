8a1cce66ab5efc4a5247666b45779a2df5bbb842
maf248 <maf_248@hotmail.com>
2021-05-07 11:19:03 -0300

    Agregue serve-favicon y el icono de Digital Now al calculador

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 
@@ -16,7 +17,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(favicon(path.join(__dirname, '..', 'public', 'images', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, '../public')));
+
 
 app.use('/', indexRouter);
 
