683ef0090c285166f741de8e9a115568afc7c340
maf248 <maf_248@hotmail.com>
2021-05-20 12:03:35 -0300

    Eliminé 2 archivos provisorios que utilicé para implementar los sub-cajones de "servicios de implementacion" y "servicios de mantenimiento".

--- src/views/Untitled-1.js
+++
@@ -1,5 +0,0 @@
-
-/*Despues, borrar: (con IDS capturados tambien)*/
-<ul id="result-implementation-details" style="list-style-type: none"></ul>
-
-<ul id="result-maintenance-details" style="list-style-type: none"></ul>
